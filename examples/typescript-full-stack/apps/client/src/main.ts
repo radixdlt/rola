7869a4a65b1d27a7241a0db6fc92072acb9edfe4
Dawid Sowa <dawid.sowa@rdx.works>
2023-09-05 11:12:12 +0200

    feat: use `@radixdlt/rola` + update to RCNet V3

--- examples/typescript-full-stack/apps/client/src/main.ts
+++ examples/typescript-full-stack/apps/client/src/main.ts
@@ -1,12 +1,13 @@
-import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import radixLogo from "/radix-icon_128x128.png";
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import radixLogo from '/radix-icon_128x128.png'
 import {
   DataRequestBuilder,
   RadixDappToolkit,
-} from "@radixdlt/radix-dapp-toolkit";
+  RadixNetwork,
+} from '@radixdlt/radix-dapp-toolkit'
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://github.com/radixdlt/radix-dapp-toolkit" target="_blank">
       <img src="${radixLogo}" class="logo" alt="Radix logo" />
@@ -21,43 +22,43 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
       Click on the Radix and TypeScript logos to learn more
     </p>
   </div>
-`;
+`
 
 const radixDappToolkit = RadixDappToolkit({
   dAppDefinitionAddress:
-    "account_tdx_d_12xxwkx4fmz680e9wz8atdnyslr9vt7x9qvcfxhtqfnpfhxyjzwtyna",
-  networkId: 13,
-});
+    'account_tdx_e_1285lfp3kwjnyu5esdsy7u9j0482tsw8fj3tnj95gjsgj6qa23yarx8',
+  networkId: RadixNetwork.RCnetV3,
+})
 
 // Clear the dApp state for example purposes
 setTimeout(() => {
-  radixDappToolkit.disconnect();
-});
+  radixDappToolkit.disconnect()
+})
 
 radixDappToolkit.walletApi.setRequestData(
   DataRequestBuilder.persona().withProof(),
   DataRequestBuilder.accounts().atLeast(1).withProof()
-);
+)
 
 const getChallenge: () => Promise<string> = () =>
-  fetch("http://localhost:3000/create-challenge")
+  fetch('http://localhost:3000/create-challenge')
     .then((res) => res.json())
-    .then((res) => res.challenge);
+    .then((res) => res.challenge)
 
-radixDappToolkit.walletApi.provideChallengeGenerator(getChallenge);
+radixDappToolkit.walletApi.provideChallengeGenerator(getChallenge)
 
-const rolaResultElement = document.getElementById("rola-result")!;
+const rolaResultElement = document.getElementById('rola-result')!
 
 radixDappToolkit.walletApi.dataRequestControl(async ({ proofs }) => {
-  const { valid } = await fetch("http://localhost:3000/verify", {
-    method: "POST",
+  const { valid } = await fetch('http://localhost:3000/verify', {
+    method: 'POST',
     body: JSON.stringify(proofs),
-    headers: { "content-type": "application/json" },
-  }).then((res): Promise<{ valid: boolean }> => res.json());
+    headers: { 'content-type': 'application/json' },
+  }).then((res): Promise<{ valid: boolean }> => res.json())
 
   rolaResultElement.innerHTML = JSON.stringify(
     proofs.map((item) => ({ ...item, rolaVerified: valid })),
     null,
     2
-  );
-});
+  )
+})
