cd820cca0cb7dbe70137b9b68e0a117b388dfbb4
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-11-29 14:51:33 -0300

    fixing tests

--- python/tests/test_helpers.py
+++ python/tests/test_helpers.py
@@ -3,7 +3,7 @@ from rola.utils.helpers import create_public_key_hash
 
 def test_create_public_key_hash():
     public_key = "a6b8a053f51c1f945317bef5f5344321783b243821e919448c5963b9a8a20552"
-    expected_hash = "e811614cac9300178b0c7ccf9930b32f8e02df26e035390100dc5468e4"
+    expected_hash = "31eeca0fe8fb2d06fe3b4006bf0c10b1942488e529282fde64f3954daf"
     assert create_public_key_hash(public_key) == expected_hash
 
 
