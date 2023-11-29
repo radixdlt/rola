f4ab383e7b84d880b499f4c895aae436e7898bf8
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-11-29 17:16:23 -0300

    fixing last unit test

--- python/tests/test_helpers.py
+++ python/tests/test_helpers.py
@@ -4,7 +4,7 @@ from rola.utils.helpers import create_public_key_hash
 def test_create_public_key_hash():
     public_key = "a6b8a053f51c1f945317bef5f5344321783b243821e919448c5963b9a8a20552"
     expected_hash = "31eeca0fe8fb2d06fe3b4006bf0c10b1942488e529282fde64f3954daf"
-    assert create_public_key_hash(public_key) == expected_hash
+    assert create_public_key_hash(bytes.fromhex(public_key)) == expected_hash
 
 
 def test_create_key_with_invalid_hex():
