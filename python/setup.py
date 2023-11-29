30e011cd2e77a992202f17e42363cec23303250b
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-11-29 16:42:43 -0300

    updating the readme and the setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,17 +1,15 @@
-import pipenv
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
-def load_dependencies(dev=False):
-    # Load dependencies from Pipfile
-    requirements = pipenv.convert.from_pipfile()
-    if dev:
-        return [str(req) for req in requirements["dev-packages"]]
-    return [str(req) for req in requirements["packages"]]
+def parse_requirements(filename):
+    with open(filename) as f:
+        return f.read().splitlines()
 
+
+requirements = parse_requirements('requirements.txt')
 
 setup(
     name="rola",
@@ -22,4 +20,5 @@ setup(
     url="https://github.com/radixdlt/rola",
     packages=find_packages(),
     python_requires=">=3.8",
+    install_requires=requirements
 )
