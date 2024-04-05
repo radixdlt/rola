0315698605a50cf9c5ac09c0a446b46c4c1b30af
Dawid Sowa <dawid.sowa@rdx.works>
2024-04-05 15:27:12 +0200

    feat: use `@noble/curves` instead of `elliptic`

--- typescript/src/helpers/verify-proof.ts
+++ typescript/src/helpers/verify-proof.ts
@@ -1,7 +1,7 @@
 import type { Result } from 'neverthrow'
 import { err, ok } from 'neverthrow'
-import { curve25519 } from '../crypto/curve25519'
-import { secp256k1 } from '../crypto/secp256k1'
+import { secp256k1 } from '@noble/curves/secp256k1'
+import { ed25519 } from '@noble/curves/ed25519'
 import type { SignedChallenge } from '../types'
 
 const supportedCurves = new Set(['curve25519', 'secp256k1'])
@@ -18,19 +18,18 @@ export const verifyProofFactory =
       let isValid = false
 
       if (input.proof.curve === 'curve25519') {
-        const publicKey = curve25519.keyFromPublic(
+        isValid = ed25519.verify(
+          input.proof.signature,
+          signatureMessageHex,
           input.proof.publicKey,
-          // @ts-ignore: incorrect type definition in EC lib
-          'hex',
         )
-        isValid = publicKey.verify(signatureMessageHex, input.proof.signature)
       } else {
         const signature = input.proof.signature.slice(2)
-        const r = signature.slice(0, 64)
-        const s = signature.slice(64, 128)
-        isValid = secp256k1
-          .keyFromPublic(input.proof.publicKey, 'hex')
-          .verify(signatureMessageHex, { r, s })
+        isValid = secp256k1.verify(
+          signature,
+          signatureMessageHex,
+          input.proof.publicKey,
+        )
       }
       return isValid ? ok(undefined) : err({ reason: 'invalidSignature' })
     } catch (error: any) {
