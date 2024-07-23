88592f6e9e17559d31e3320d9f6c7228147e18bb
Santiago Baldassin <santiago.baldassin@rdx.works>
2024-07-23 10:10:20 -0300

    fixing unit test

--- python/tests/test_gateway.py
+++ python/tests/test_gateway.py
@@ -3,18 +3,6 @@ from rola.exceptions.gateway import EntityNotFound
 from rola.utils.gateway import GatewayMetadataProvider
 
 
-def test_gateway_returns_entity_owner_for_mainnet():
-    gateway_metadata_provider = GatewayMetadataProvider.for_mainnet()
-    entity_owners = gateway_metadata_provider.entity_owner(
-        address="identity_tdx_2_12gc7ajs0araj6ph78dqqd0cvzzcegfygu55jst77vnee2nd05vp8wc"
-    )
-    assert (
-        entity_owners[0]["hash_hex"]
-        == "31eeca0fe8fb2d06fe3b4006bf0c10b1942488e529282fde64f3954daf"
-    )
-    assert entity_owners[0]["key_hash_type"] == "EddsaEd25519"
-
-
 def test_gateway_returns_entity_owner_for_stokenet():
     gateway_metadata_provider = GatewayMetadataProvider.for_stokenet()
     entity_owners = gateway_metadata_provider.entity_owner(
@@ -35,11 +23,3 @@ def test_gateway_raises_400_error_when_invalid_address():
         assert e.response.status_code == 400
 
 
-def test_gateway_raises_entity_not_found_error():
-    gateway_metadata_provider = GatewayMetadataProvider.for_stokenet()
-    try:
-        gateway_metadata_provider.entity_owner(
-            address="validator_rdx1s0g5uuw3a7ad7akueetzq5lpejzp9uw5glv2qnflvymgendvepgduj"
-        )
-    except EntityNotFound:
-        assert True
